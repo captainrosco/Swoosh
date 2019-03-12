205a90d831a54315dcae2b89413c0e69289c655d
Daron Short <daron.short@hotmail.com>
2019-03-12 12:35:43 -0500

    Setup Intent to pass league parameters through the app.

--- app/src/main/java/com/example/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh/LeagueActivity.kt
@@ -4,16 +4,45 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        var selectedLeague = ""
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
 
         fun leagueNextClick(view: View) {
-            val skillActivity = Intent(this, SkillActivity::class.java)
-            startActivity(skillActivity)
+            if(selectedLeague != ""){
+                val skillActivity = Intent(this, SkillActivity::class.java)
+                skillActivity.putExtra(EXTRA_LEAGUE, selectedLeague)
+                startActivity(skillActivity)
+            } else {
+                Toast.makeText(this, "Select one option", Toast.LENGTH_SHORT).show()
+            }
         }
+
+        fun onMenClick(view: View){
+            womenButton.isChecked = false
+            coEdButton.isChecked = false
+            selectedLeague = "men"
+        }
+
+        fun onWomenClick(view: View){
+            menButton.isChecked = false
+            coEdButton.isChecked = false
+            selectedLeague = "women"
+        }
+
+        fun onCoClick(view: View){
+            menButton.isChecked = false
+            womenButton.isChecked = false
+            selectedLeague = "co-ed"
+        }
+
     }
 }
